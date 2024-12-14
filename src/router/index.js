d173ba06dca87e82dafd3367d95bdbf88fc1a54e
EdisonTamayo127 <edisontamayo13@gmail.com>
2024-12-15 00:33:05 +0800

    last

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
 import UserDashboard from '@/views/auth/UserDashboard.vue'
 import { supabase } from "@/supabase"; // Import Supabase client
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
